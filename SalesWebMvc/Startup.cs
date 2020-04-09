9c7eea7b950dea5c632187b4c524a484fa05063e
Jose Luis Boesing <jlboesing@gmail.com>
2020-04-09 13:55:41 -0300

    update seller and custom service exception

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Data;
 using SalesWebMvc.Services;
 
